dff200084f560e81c302c5c7865f29eceaac0cf7
sonncui <740548795@qq.com>
2016-08-15 20:33:47 +0800

    [cc] refactor.

--- lib/number_transfer.rb
+++ lib/number_transfer.rb
@@ -1,65 +1,7 @@
+require_relative './transfer_helper'
+
 class NumberTransfer
-
-  NUMBER_CHARACTER = {
-    1 => 'one',
-    2 => 'two',
-    3 => 'three',
-    4 => 'four',
-    5 => 'five',
-    6 => 'six',
-    7 => 'seven',
-    8 => 'eight',
-    9 => 'nine',
-    10 => 'ten',
-    11 => 'eleven',
-    12 => 'twelve',
-    13 => 'thirteen',
-    14 => 'fourteen',
-    15 => 'fifteen',
-    16 => 'sixteen',
-    17 => 'seventeen',
-    18 => 'eighteen',
-    19 => 'nineteen'
-  }
-
-  DOUBLE_NUMBER = {
-    2 => 'twenty',
-    3 => 'thirty',
-    4 => 'fourty',
-    5 => 'fifty',
-    6 => 'sixty',
-    7 => 'seventy',
-    8 => 'eighty',
-    9 => 'ninety'
-  }
-
-  UNIT_NUMBER = {
-    0 => '',
-    1 => 'thousand',
-    2 => 'million',
-    3 => 'trillion'
-  }
-
-  HANDRED = 'handred'
-
-  def transfer_inner(number)
-    character_string = ''
-    handred_number = number % 1000 / 100
-    double_number = number % 100 / 10
-    single_number = number % 10
-    if handred_number > 0
-      character_string = NUMBER_CHARACTER[handred_number]+ ' ' + HANDRED + ' '
-    end
-    if double_number > 1
-      character_string += DOUBLE_NUMBER[double_number] + ' '
-      if single_number > 0
-        character_string += NUMBER_CHARACTER[single_number]
-      end
-    elsif single_number > 0
-      character_string += NUMBER_CHARACTER[double_number*10+single_number]
-    end
-    return character_string.strip
-  end
+  include Helper
 
   def transfer(number)
     number = number.to_i
@@ -79,5 +21,25 @@ class NumberTransfer
       unit_count = unit_count + 1
     end
     return outer_string.strip
+  end
+
+  private
+  def transfer_inner(number)
+    character_string = ''
+    handred_number = number % 1000 / 100
+    double_number = number % 100 / 10
+    single_number = number % 10
+    if handred_number > 0
+      character_string = NUMBER_CHARACTER[handred_number]+ ' ' + HANDRED + ' '
+    end
+    if double_number > 1
+      character_string += DOUBLE_NUMBER[double_number] + ' '
+      if single_number > 0
+        character_string += NUMBER_CHARACTER[single_number]
+      end
+    elsif single_number > 0
+      character_string += NUMBER_CHARACTER[double_number*10+single_number]
+    end
+    return character_string.strip
   end
 end
---
+++ lib/transfer_helper.rb
@@ -0,0 +1,43 @@
+module Helper
+  NUMBER_CHARACTER = {
+    1 => 'one',
+    2 => 'two',
+    3 => 'three',
+    4 => 'four',
+    5 => 'five',
+    6 => 'six',
+    7 => 'seven',
+    8 => 'eight',
+    9 => 'nine',
+    10 => 'ten',
+    11 => 'eleven',
+    12 => 'twelve',
+    13 => 'thirteen',
+    14 => 'fourteen',
+    15 => 'fifteen',
+    16 => 'sixteen',
+    17 => 'seventeen',
+    18 => 'eighteen',
+    19 => 'nineteen'
+  }
+
+  DOUBLE_NUMBER = {
+    2 => 'twenty',
+    3 => 'thirty',
+    4 => 'fourty',
+    5 => 'fifty',
+    6 => 'sixty',
+    7 => 'seventy',
+    8 => 'eighty',
+    9 => 'ninety'
+  }
+
+  UNIT_NUMBER = {
+    0 => '',
+    1 => 'thousand',
+    2 => 'million',
+    3 => 'trillion'
+  }
+
+  HANDRED = 'handred'
+end
